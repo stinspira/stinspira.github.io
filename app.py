4fb90b15d599e366a8bd8bf49d34c6da4c9bd522
fregu856 <fregu856@student.liu.se>
2018-04-06 12:10:11 +0200

    >

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/intervjuer", methods=["GET", "POST"])
+def intervjuer():
+    return render_template("intervjuer.html")
+
 if __name__ == '__main__':
     app.run()
